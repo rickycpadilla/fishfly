81702cd3b96267dda8f883a77867cb5c9cbc339d
Ricky Padilla <rickycpadilla@gmail.com>
2016-06-23 11:20:28 -0600

    results listed by distance to user

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ function getWeather(lat, lng, id){
   $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lng + "&APPID=4756af9614c9971a6c4c4b17ef4630fe", function(data){
     if(data.main.temp){
       var weatherCond = data.weather[0].description.replace(/ /g,"");
-      console.log(weatherCond);
       var icon = $("<img src='/images/icons/" + weatherCond + ".png' style='width: 35px; margin-right:5px' class='icon'/>");
       icon.appendTo("#leftw" + id);
       //$("<span>" + weatherCond + " </span>").appendTo("#weath" + id);
@@ -53,6 +52,17 @@ function markerMsg(marker, message) {
 
 function getBaseData() {
   $.getJSON( "https://firebasestorage.googleapis.com/v0/b/fishfly-53334.appspot.com/o/spots.json?alt=media&token=3fc71264-588c-4bc6-a5e6-be2d968d8eb5", function(data){
+// adding distance comparisons
+    for(var i=0; i<data.length; i++){
+      var dist = distance(userLat, userLng, data[i].lat, data[i].lng);
+      data[i]["dist"] = dist;
+    }
+    var data = data.slice(0);
+    data.sort(function(a,b) {
+      return a.dist - b.dist;
+    });
+
+// Original function below
     for(var i = 0; i<data.length; i++){
       $("<div id='div"+data[i].id+"'></div>").appendTo("#datapage");
       getWeather(data[i].lat, data[i].lng, data[i].id);
@@ -62,34 +72,33 @@ function getBaseData() {
       $("<div style='width: 100%; height: 60px; background-color: black; margin: -64px 0px 0px 0px; position: absolute; opacity: 0.6' class='blackbox'></div>").appendTo("#div" +data[i].id);
       $("<div style='position:absolute; width: 100%; margin-top: -60px; color: white' id='weath" + data[i].id +"'></div>").appendTo("#div" +data[i].id);
       $("<div style='float: left; width: 100px' id='leftw" + data[i].id + "'></div>").appendTo("#weath" + data[i].id);
-      $("<div style='float: right; width: 115px; font-size: 12px; font-weight: light; padding-top: 5px; margin-right:30px; text-align: right' id='rightw" + data[i].id + "'></div>").appendTo("#weath" + data[i].id);
+      $("<div style='float: right; width: 116px; font-size: 12px; font-weight: light; padding-top: 5px; margin-right:30px; text-align: right' id='rightw" + data[i].id + "'></div>").appendTo("#weath" + data[i].id);
       $("<h2 id='" + data[i].id +"'>" + data[i].name + " </h2>").appendTo("#div" +data[i].id);
       $("<h4 id='" + data[i].id +"'>" + data[i].loc + " </h4>").appendTo("#div" +data[i].id);
       makeMarkers(data[i].lat, data[i].lng, data[i].name);
-      console.log(data[i].name);
     }
   })
 };
 
-
-// come back to this when ready to sort based on distance
-function distance(lat1, lon1, lat2, lon2) {
+function distance(usrLat, usrLng, locLat, locLng) {
   var p = 0.017453292519943295;    // Math.PI / 180
   var c = Math.cos;
-  var a = 0.5 - c((lat2 - lat1) * p)/2 +
-          c(lat1 * p) * c(lat2 * p) *
-          (1 - c((lon2 - lon1) * p))/2;
+  var a = 0.5 - c((locLat - usrLat) * p)/2 +
+          c(usrLat * p) * c(locLat * p) *
+          (1 - c((locLng - usrLng) * p))/2;
 
-  console.log(12742 * Math.asin(Math.sqrt(a)))// 2 * R; R = 6371 km
+  return (12742 * Math.asin(Math.sqrt(a)))// 2 * R; R = 6371 km
 }
 
+var userLat;
+var userLng;
 var map;
 function initMap(){
   var userInput = $("#destination").val();
   var geocoder = new google.maps.Geocoder();
   geocoder.geocode({ 'address' : userInput }, function(results, status) {
-      var userLat = [];
-      var userLng = [];
+      userLat = [];
+      userLng = [];
   if(status == "ZERO_RESULTS") {
     //Indicate to user no location has been found
     console.log("nope!");
@@ -100,7 +109,7 @@ function initMap(){
     center: {lat: userLat[0], lng: userLng[0]},
     zoom: 9
   });
-  distance(userLat[0], userLng[0], 39.249, -106.348111)
+  // distance(userLat[0], userLng[0], 39.249, -106.348111)
 }
 })
 };
@@ -112,8 +121,8 @@ $(window).bind("load", function () {
 $( "#target" ).submit(function( event ) {
   event.preventDefault();
   $("#datapage").html("");
-  var userLat = [];
-  var userLng = [];
+  userLat = [];
+  userLng = [];
   var userInput = $("#destination").val();
   var geocoder = new google.maps.Geocoder();
   geocoder.geocode({ 'address' : userInput }, function(results, status) {
